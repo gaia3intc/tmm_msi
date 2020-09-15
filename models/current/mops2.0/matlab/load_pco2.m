6c781a73cbde071d993fc8503f24853d66c66fe1
Tatsuro Tanioka <tanio003@umn.edu>
2020-09-15 17:00:44 -0500

    edited load_pco2.m for mops

--- models/current/mops2.0/matlab/load_pco2.m
+++ models/current/mops2.0/matlab/load_pco2.m
@@ -25,14 +25,16 @@ load(boxFile,'izBox','nb');
 load(profilesFile,'Irr');
 
 if ~useTimeVaryingPrescribedCO2
-    timeFile='atm_output_time.txt';
-    [hdr,tdat]=hdrload(timeFile);
-    T = tdat(:,2);
-    nt=length(T);
+    if exist('atm_output_time.txt', 'file') == 1;
+       timeFile='atm_output_time.txt';
+       [hdr,tdat]=hdrload(timeFile);
+       T = tdat(:,2);
+       nt=length(T);
 
-   pCO2LogFile='pCO2atm_output.bin';
-   diagData=read_binary(pCO2LogFile,[],'float64');
-   pCO2atm= diagData;
+       pCO2LogFile='pCO2atm_output.bin';
+       diagData=read_binary(pCO2LogFile,[],'float64');
+       pCO2atm= diagData;
+   end
 else
     if strcmp(co2Scenario,'historical')
         co2File='co2_HG11_extrap.dat'; % historical data from Heather Graven
@@ -43,24 +45,27 @@ else
         T=pCO2_atm(:,1);
         pCO2atm=pCO2_atm(:,2);
 end
-ncid=netcdf.create('pco2.nc','CLOBBER');
-dimidt = netcdf.defDim(ncid,'Year',length(T));
-date_ID=netcdf.defVar(ncid,'Year','double',[dimidt]);
-pCO2_ID=netcdf.defVar(ncid,'pco2','double',[dimidt]);
-netcdf.endDef(ncid);
-netcdf.putVar(ncid,date_ID,T);
-netcdf.putVar(ncid,pCO2_ID,pCO2atm);
-netcdf.close(ncid);
-
+if exist('pCO2atm','var') == 1; %
+   ncid=netcdf.create('pco2.nc','CLOBBER');
+   dimidt = netcdf.defDim(ncid,'Year',length(T));
+   date_ID=netcdf.defVar(ncid,'Year','double',[dimidt]);
+   pCO2_ID=netcdf.defVar(ncid,'pco2','double',[dimidt]);
+   netcdf.endDef(ncid);
+   netcdf.putVar(ncid,date_ID,T);
+   netcdf.putVar(ncid,pCO2_ID,pCO2atm);
+   netcdf.close(ncid);
+end
 % Making co2 airsea flux 2D data 
-avg_timeFile='time_average_output_time.txt';
-[hdravg,diagnostic_output_time]=hdrload(avg_timeFile);
-Tavg = diagnostic_output_time(:,2);
-Ib=find(izBox==1);
-nbb=length(Ib);
-diagFile='co2airseaflux_surf.bin';
-diagData=read_binary(diagFile,[],'float64');
-nt=length(diagData)/nbb;
-diagData=reshape(diagData,[nbb nt]);
-Diag=matrixToGrid(diagData,Ib,boxFile,gridFile);
-write2netcdf(['co2airseaflux.nc'],Diag,x,y,[],Tavg,'co2airseaflux','mmolC/m2/timestep')
+if exist('time_average_output_time.txt', 'file') == 1;
+   avg_timeFile='time_average_output_time.txt';
+   [hdravg,diagnostic_output_time]=hdrload(avg_timeFile);
+   Tavg = diagnostic_output_time(:,2);
+   Ib=find(izBox==1);
+   nbb=length(Ib);
+   diagFile='co2airseaflux_surf.bin';
+   diagData=read_binary(diagFile,[],'float64');
+   nt=length(diagData)/nbb;
+   diagData=reshape(diagData,[nbb nt]);
+   Diag=matrixToGrid(diagData,Ib,boxFile,gridFile);
+   write2netcdf(['co2airseaflux.nc'],Diag,x,y,[],Tavg,'co2airseaflux','mmolC/m2/timestep')
+end
